9009a73b7357b19028d8f3fac701c653cd26517a
Oktif <47919676+Oktif@users.noreply.github.com>
2023-01-23 21:28:29 +0100

    V 0.2
    - Chatbot can now change topics and categories
    - Added dummy categories

--- main.py
+++ main.py
@@ -5,25 +5,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 import random
 import wikipedia
 
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download('punkt')
-
-page = input("What topic you want to talk about?")
-# try:
-#     p = wikipedia.page(page)
-# except wikipedia.DisambiguationError as e:
-#     s = random.choice(e.options)
-#     p = wikipedia.page(s)
-
-try:
-    p = wikipedia.page(page)
-except wikipedia.DisambiguationError as e:
-    random = random.choice(e.options)
-    page = random
-print("chosen: ", page)
-text = wikipedia.page(page).content
-
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+# nltk.download('punkt')
 
 lemmatizer = WordNetLemmatizer()
 
@@ -50,16 +34,187 @@ def process(text, question):
   values_flat = values.flatten()
   values_flat.sort()
   coeff = values_flat[-2]
-  if coeff > 0.1:
+  if coeff > 0.2:
     return sentence_tokens[index]
 
 
 while True:
-  question = input("Hi, what do you want to know?\n")
-  output = process(text, question)
-  if output:
-    print(output)
-  elif question=='quit':
-    break
-  else:
-    print("I don't know.")
+    print('Im simple bot, and can only talk about selected topics.'
+          'What you want to talk about?'
+          'type "quit" to end conversation')
+    category = input('chose either animals or vegetables').lower()
+    if category == 'animals':
+            while True:
+                print(f'{category} chosen, if you want to change category, type "category"')
+                talking = input(f'What {category} interests you?').lower()
+                if talking == 'category':
+                    break
+                else:
+                    while True:
+                        question = input(f'You want to ask about {talking}? (If not type "question" to change it)').lower()
+                        if question == 'question':
+                            break
+                        else:
+                            page = (category + ' ' + talking)
+                            try:
+                                p = wikipedia.page(page)
+                            except wikipedia.DisambiguationError as e:
+                                random = random.choice(e.options)
+                                page = random
+                            text = wikipedia.page(page).content
+                            while True:
+                                question2 = input('Ask away!, if you want to change topic, type "topic"\n')
+                                output = process(text, question2)
+                                if output:
+                                    print(output)
+                                elif question2 == 'topic':
+                                    break
+                                else:
+                                    print("I don't know.")
+
+    elif category == 'vegetables':
+        print("vegetables chosen")
+
+    elif category == 'cats':
+        print("cats")
+
+    elif category == 'dogs':
+        print("cats")
+
+    elif category == 'pigeons':
+        print("cats")
+
+    elif category == 'quit':
+        print("bye!")
+        break
+
+    else:
+        print("wrong choice!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
+#     print("Im simple bot, and can only talk about selected topics."
+#           "What you want to talk about?")
+#     category = input("chose either animals or vegetables")
+#     if category == "animals":
+#         print("Animals chosen, if you want to change topic, type : change")
+#         talking = input(f"What about {category} interests you?")
+#         if talking == 'change':
+#             pass
+#         else:
+#             question = input(f"What about {talking} you want to ask? (to change type change)")
+#             if question != "change" or "pass":
+#                 while question != "change":
+#                     page = (category+' '+talking)
+#                     print(wikipedia.summary(page))
+#                     question = input(f"What about {talking} you want to ask? (to change type pass)")
+#             elif question == "pass":
+#                 break
+#     elif category == "vegetables":
+#         print("vegetables chosen")
+#     else:
+#         print("wrong choice")
+
+
+
+
+
+
+
+# def topic():
+#     import random
+#     page = input("What topic you want to talk about?")
+#     try:
+#         p = wikipedia.page(page)
+#     except wikipedia.DisambiguationError as e:
+#         random = random.choice(e.options)
+#         page = random
+#     print("chosen: ", page)
+#     text = wikipedia.page(page).content
+#     return text
+#
+# def discussion():
+#     while True:
+#       question = input("Hi, what do you want to know?\n")
+#       output = process(text, question)
+#       if output:
+#         print(output)
+#       elif question=='quit':
+#         break
+#       else:
+#         print("I don't know.")
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
+#     print("Im simple bot, and can only talk about selected topics."
+#           "What you want to talk about?")
+#     category = input("chose either animals or vegetables")
+#     if category == "animals":
+#             while True:
+#                 print(f"{category}chosen, if you want to change category, type : change")
+#                 talking = input(f"What about {category} interests you?")
+#                 if talking == 'change':
+#                     break
+#                 else:
+#                     while True:
+#                         question = input(f"What about {talking} you want to ask? (to change topic talking)")
+#                         if question == "talking":
+#                             break
+#                         else:
+#                             page = (category + ' ' + talking)
+#                             print(wikipedia.summary(page))
+#
+#     elif category == "vegetables":
+#         print("vegetables chosen")
+#     else:
+#         print("wrong choice")
+
+
+
+
+# page = input("What topic you want to talk about?")
+# try:
+#     p = wikipedia.page(page)
+# except wikipedia.DisambiguationError as e:
+#     random = random.choice(e.options)
+#     page = random
+# #print("chosen: ", page)
+# text = wikipedia.page(page).content
+
+# while True:
+#   question = input("Hi, what do you want to know?\n")
+#   output = process(text, question)
+#   if output:
+#     print(output)
+#   elif question=='quit':
+#     break
+#   else:
+#     print("I don't know.")
